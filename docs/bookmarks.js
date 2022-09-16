8ad320c06eb0b9572bb968f5a16da4090d1d98b4
megamind <megamindbrian@gmail.com>
2022-09-16 10:38:49 -0700

    bookmarks scaffolding, needs validation, then 3 methods of conversion copied from jupyter_ops

---
+++ docs/bookmarks.js
@@ -0,0 +1,110 @@
+
+const CHUNK_SIZE = 1000
+
+const SAVE_URL = '/fileput'
+const NON_ALPHA_NUM = (/[^a-z0-9-_]/gi)
+
+const files = []
+
+async function uploadFiles(ev) {
+  if (ev.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    [...ev.dataTransfer.items].forEach((item, i) => {
+      // If dropped items aren't files, reject them
+      if (item.kind === 'file') {
+        files.push(item.getAsFile())
+      }
+    })
+  } else if (ev.dataTransfer.files) {
+    [...ev.dataTransfer.files].forEach((file, i) => {
+      files.push(file)
+    });
+  }
+
+  // TODO: why is this even necessary? why in the hell would someone ever design 2 separate interfaces for a drag and drop dataTransfer? probably because someone didn't plan ahead the first time, and someone else decided they couldn't break something else. which one is it? .items or .files? MORE CODE is obviously the only answer
+  // TODO: upload multiple files?
+  if(!files.length) {
+    return
+  }
+  const currentFile = files[0]
+  const chunks = currentFile.size / CHUNK_SIZE
+  const fileData = new Uint8Array(await currentFile.arrayBuffer())
+  let fileKey
+
+  for(let i = 0; i < Math.ceil(chunks) + 1; i++) {
+    let segmentData = fileData.slice(i * CHUNK_SIZE, i * CHUNK_SIZE + CHUNK_SIZE)
+    let putData = {
+      part: i,
+      data: Array.from(segmentData)
+    }
+    if(i == 0) {
+      putData.name = currentFile.name.replace(NON_ALPHA_NUM, '-')
+    } else {
+      putData.key = fileKey
+    }
+    let response = await fetch(SAVE_URL, {
+      method: 'PUT',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify(putData)
+    })
+    if(i == 0) {
+      fileKey = Array.from(new Uint8Array(await response.arrayBuffer()))
+          .map(c => String.fromCharCode(c)).join('')
+      debugger
+    }
+  }
+}
+
+if(typeof window != 'undefined') {
+
+  window.addEventListener('load', function () {
+    let dropzone = document.getElementById('drop_zone')
+    let inputfile = document.getElementById('upload_file')
+
+    dropzone.addEventListener('dragenter', function () {
+      dropzone.classList.add('dragging')
+    })
+
+    dropzone.addEventListener('dragleave', function () {
+      dropzone.classList.remove('dragging')
+    })
+
+    dropzone.addEventListener('dragover', function (ev) {
+      ev.preventDefault()
+    })
+
+    inputfile.addEventListener('change', function (ev) {
+      ev.preventDefault()
+      uploadFiles({
+        dataTransfer: {
+          files: inputfile.files
+        }
+      })
+    })
+
+    // TODO: not sure when this files but this doesn't appear to work
+    //   only works on an unsuccessful drap/drop? only works at window level? ambiguity everywhere
+    /*
+    dropzone.addEventListener('dragend', function (ev) {
+      ev.preventDefault()
+      dropzone.classList.remove('dragging')
+    })
+    */
+
+    dropzone.addEventListener("drop",function (ev) {
+      ev.preventDefault()
+      dropzone.classList.remove('dragging')
+      uploadFiles(ev)
+    })
+
+  })
+
+}
+
+if(typeof module != 'undefined') {
+  module.exports = {
+    SAVE_URL,
+  }
+}
---
+++ index.js
@@ -0,0 +1,110 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const { SAVE_URL, NON_ALPHA_NUM } = require('./docs/bookmarks.js')
+const INDEX = fs.readFileSync('./index.html').toString('utf-8')
+
+// todo: make dynamic
+
+function listBookmarks(bookmarks) {
+  return '<ol class="decks">' + (bookmarks || []).map(deck => {
+    return '<li><a href="' + deck.name.replace(NON_ALPHA_NUM, '-') + '.html">' + deck.name + '</a></li>'
+  }).join('\n') + '</ol>'
+}
+
+function renderIndex() {
+	let index = INDEX
+
+	let bodyTag = index.match(/<ol class="bookmarks"[\n\r.^>]*?>/i)
+	let offset = bodyTag.index
+	index = index.substring(0, offset)
+		+ listBookmarks() + index.substring(offset + bodyTag[0].length, index.length)
+
+	fs.writeFileSync(path.join(__dirname, 'docs/index.html'), index)
+}
+
+function randomKey(length = 8) {
+  return Math.random().toString(16).substr(2, length);
+}
+
+function zipToHtml() {
+  // TODO: unzip contents into memory
+
+  // TODO: import sqlite to make a smaller, more ubiquitous deck of flash cards
+
+  // TODO: animate flash card transition with 3D game engine experience
+
+  
+}
+
+
+const RECENT_UPLOADS = {}
+
+function uploadFile(req, res, next) {
+  // TODO: accept file uploads as minimal example, this took long because the machine is working against me
+  let fileKey
+  if(req.body.part == 0) {
+    fileKey = randomKey(16)
+    RECENT_UPLOADS[fileKey] = req.body.name
+  } else {
+    fileKey = req.body.key
+  }
+
+  if(req.body.data.length == 0 && req.body.part > 0) {
+    // begin converstion process
+
+  }
+
+  // i'd feel bad about looking this stuff up if there wasn't so much nuance everywhere else, is this a sign that programmers are progressively worse the longer they use a specific technology?
+  fs.writeFileSync(RECENT_UPLOADS[fileKey], Buffer.from(req.body.data), { flag: 'a' })
+
+  if(req.body.part == 0) {
+    res.setHeader('content-type', 'text/plain')
+    return res.send(fileKey)
+  } else {
+    res.setHeader('content-type', 'text/plain')
+    return res.send('Ok')
+  }
+}
+
+function loadContent(req, res, next) {
+  let pathname = req.originalUrl
+  if(pathname == '/') {
+    pathname = '/index.html'
+  }
+  const filename = path.join(__dirname, '/docs/', req.originalUrl)
+  if(pathname == '/index.html' && !fs.existsSync(filename)) {
+    renderIndex()
+  }
+  if(fs.existsSync(filename)) {
+    return res.sendFile(filename)
+  }
+  return next()
+}
+
+// TODO: PUT 405 error from NPM's `live-server`, no workaround listed, too complicated, not worth fighting about or fixing, good for simply static pages, not good for developing a server-side function
+
+
+function startServer() {
+  const app = express()
+  app.enable('etag')
+  app.set('etag', 'weak')
+  app.use(express.json())
+  app.use(loadContent)
+  app.put(SAVE_URL, uploadFile)
+
+  // TODO: nuance, none of these options work anymore, everything is broken, npm is infected or too much complexity to keep track of? why do so many tutorials have bad instructions? I must be living in some alternate reality of hell, https://stackoverflow.com/questions/10867052/cannot-serve-static-files-with-express-routing-and-no-trailing-slash
+  
+  //app.use(express.static(path.resolve(__dirname, './docs/')))
+  //app.all('*', function(req, res) { res.redirect('/main/'); });
+  //app.use('/', express.static(__dirname+'/docs'));
+
+  const { createServer } = require('http')
+  const httpServer = createServer(app).listen(8080)
+  
+}
+
+module.exports = {
+  startServer,
+  renderIndex,
+}
